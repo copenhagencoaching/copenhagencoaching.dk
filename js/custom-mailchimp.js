915d6aaa9fa09f30a60af78e6e8586d1d58f0484
Carolineolivia <carolineolivia@prolike.io>
2020-09-04 11:55:27 +0200

    New affirmation dates

--- js/custom-mailchimp.js
+++
@@ -1,4 +0,0 @@
-
-// Add a surrounding <div> for all the ish don't want to keep once submission is successful
-
-<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script>
